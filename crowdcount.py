e8297f4a96392842ee1f86ef67642fb47b53e73e
inqs <mauinkyu@gmail.com>
2022-03-29 19:09:43 +0900

    firebase 스토리지에서 pi에서 찍은 사진을 이용하여  counting counting 값을 firestore DB에 저장 코드 10초마다 반복

--- crowdcount.py
+++ crowdcount.py
@@ -4,46 +4,76 @@ from cv2 import dnn
 import pylab
 import matplotlib.pyplot as plt
 import time
-
+import threading
+import firebase_admin
+from firebase_admin import credentials, storage, firestore
 
 #pre-trained된 모델 경로
 model_path = 'model\\'
 
 if __name__ == "__main__":
 
-    #계산하고자 하는 이미지
-    img = r'image\student1.jpg'
+    # firebase 키
+    cred = credentials.Certificate("picamera-test-firebase-adminsdk-4clhg-e41ffb9866.json")
+    app = firebase_admin.initialize_app(cred, {'storageBucket': f"picamera-test.appspot.com"})
+    db = firestore.client()
 
-    img_ori = cv2.imread(img)
-    plt.figure(1)
-    plt.imshow(img_ori)
-    plt.axis('off')
-    pylab.show()
+    while (True):
+        # 저장소 access
+        bucket = storage.bucket()
+        blob = bucket.get_blob('image_store/image.jpg')  # blob
+        arr = np.frombuffer(blob.download_as_string(), np.uint8)  # array of bytes
+
+        img_ori = cv2.imdecode(arr, cv2.COLOR_BGR2BGR555)  # Firebase에서 불러온 사진
+
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
+
+        #계산하고자 하는 이미지
+        # img = r'image\abc.jpg'
+        # img_ori = cv2.imread(img)
+
+        # # 테스트용 원본이미지 출력
+        # plt.figure(1)
+        # plt.imshow(img_ori)
+        # plt.axis('off')
+        # pylab.show()
 
 
-    #RGB값 일부를 제외시켜 이미지 처리를 용이하게 함
-    blob = dnn.blobFromImage(img_ori, 1, (1024, 768), (0, 0, 0), True)
+        #RGB값 일부를 제외시켜 이미지 처리를 용이하게 함
+        blob = dnn.blobFromImage(img_ori, 1, (1024, 768), (0, 0, 0), True)
 
-    current = time.localtime()
+        current = time.localtime()
 
-    print("현재 시각:", "%04d/%02d/%02d %02d:%02d:%02d" % (current.tm_year, current.tm_mon, current.tm_mday, current.tm_hour, current.tm_min, current.tm_sec)
-)
-    #이미지 네트워크가 담긴 prototxt와 protxt 데이터를 학습시킨 caffemodel을 참조
-    net = dnn.readNetFromCaffe(model_path + 'mcnn_model.prototxt', model_path + 'mcnn_model.caffemodel')
+        print("==============================")
+        print("현재 시각:", "%04d/%02d/%02d %02d:%02d:%02d" % (current.tm_year, current.tm_mon, current.tm_mday, current.tm_hour, current.tm_min, current.tm_sec)
+    )
+        #이미지 네트워크가 담긴 prototxt와 protxt 데이터를 학습시킨 caffemodel을 참조
+        net = dnn.readNetFromCaffe(model_path + 'mcnn_model.prototxt', model_path + 'mcnn_model.caffemodel')
 
-    t = time.time()
-    net.setInput(blob)
-    density = net.forward()
-    elapsed = time.time() - t
+        t = time.time()
+        net.setInput(blob)
+        density = net.forward()
+        elapsed = time.time() - t
 
-    print('이미치 처리 소요 시간: %.4f 초' % elapsed)
+        print('이미치 처리 소요 시간: %.4f 초' % elapsed)
 
-    density = density/1000.0
-    crowd_count = np.sum(density[:])        #계산된 군중 수
+        density = density/1000.0
+        crowd_count = np.sum(density[:])        #계산된 군중 수
 
-    print("계산된 군중 수: ", int(crowd_count))
+        print("계산된 인원 수: ", int(crowd_count))
 
-    plt.figure(1)
-    plt.imshow(density[0, 0, :, :])
-    plt.axis('off')
-    pylab.show()
+        # firestore에 데이터 추가
+        doc_ref = db.collection(u'counting').document(u'number')
+        doc_ref.set({
+            u'count': int(crowd_count)
+        })
+
+        # #테스트용 이미지 출력
+        # plt.figure(1)
+        # plt.imshow(density[0, 0, :, :])
+        # plt.axis('off')
+        # pylab.show()
+
+        #10초마다 반복
+        time.sleep(10)
